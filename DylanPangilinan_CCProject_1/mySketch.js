9ac123dcd1c6e248957e801f81e0eb4967d8117c
dylpang <dap544@nyu.edu>
2021-03-17 21:41:37 -0400

    added shapes

--- DylanPangilinan_CCProject_1/mySketch.js
+++ DylanPangilinan_CCProject_1/mySketch.js
@@ -1,10 +1,19 @@
 // Dylan Pangilinan
+// Project 1
+
+let bomb;
 
 function setup() {
 	createCanvas(1000, 1000);
-	background(100);
+	background("#F5F5DC");
 }
 
+
 function draw() {
-	
+	bomb = new Explosive(0, 0);
+	bomb.display();
+
+
 }
+
+
---
+++ DylanPangilinan_CCProject_1/shapes.js
@@ -0,0 +1,35 @@
+
+class Explosive{
+	constructor(x, y){
+		this.xpos = x;
+		this.ypos = y;
+	}
+
+	display() {
+		//left leg
+		noStroke();
+		fill("#FF9A25");
+		ellipse(this.xpos + 475, this.ypos + 545, 45, 35);
+		// top
+		strokeWeight(2);
+		stroke(0);
+		fill(100);
+		rectMode(CENTER);
+		rect(500, 450, 50, 25, 20);
+		noStroke();
+		// outer circle
+		fill(0);
+		circle(this.xpos + 500,this.ypos + 500, 100);
+
+		//eyes	
+		fill(255);
+		ellipse(this.xpos + 485, this.ypos + 485, 15,  25);
+		ellipse(this.xpos + 515, this.ypos + 485, 15,  25);
+		//right leg
+		fill("#FF9A25");
+		ellipse(this.xpos + 525, this.ypos + 545, 45, 35);
+
+
+	}
+
+}	
